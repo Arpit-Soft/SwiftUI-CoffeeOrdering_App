190107c7cafa0fbae02252af9fdc84a79fb17c65
Arpit Dixit <arpitd@dewsolutions.in>
2021-06-24 22:37:08 +0530

    Initial Commit

---
+++ SwiftUI-CoffeeOrdering_App/SwiftUI_CoffeeOrdering_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CoffeeOrdering_AppApp.swift
+//  SwiftUI-CoffeeOrdering_App
+//
+//  Created by Arpit Dixit on 24/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CoffeeOrdering_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
